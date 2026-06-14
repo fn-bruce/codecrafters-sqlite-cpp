e08faa96fcbec6750434812e9a21249f4dc13a44
Bruce Nguyen <bruceleenguyen95@gmail.com>
2026-06-14 01:04:48 -0600

    update

--- src/database/record.hpp
+++ src/database/record.hpp
@@ -7,9 +7,26 @@
 
 #include "utils.hpp"
 
-using Value = std::variant<std::monostate, int8_t, int16_t, std::array<int8_t, 3>, int32_t, std::array<int8_t, 5>, int64_t,
-                           double, std::vector<int8_t>, std::string>;
+using Value =
+    std::variant<std::monostate, int8_t, int16_t, std::array<int8_t, 3>,
+                 int32_t, std::array<int8_t, 5>, int64_t, double,
+                 std::vector<int8_t>, std::string>;
 using Values = std::vector<Value>;
+
+enum class SerialType : uint64_t {
+  NIL = 0,
+  BIT_8 = 1,
+  BIT_16 = 2,
+  BIT_24 = 3,
+  BIT_32 = 4,
+  BIT_48 = 5,
+  BIT_64 = 6,
+  FLOAT = 7,
+  ZERO = 8,
+  ONE = 9,
+  RESERVED_1 = 10,
+  RESERVED_2 = 11,
+};
 
 struct Record {
   Values values{};
@@ -22,12 +39,12 @@ inline Record read_record(std::ifstream &db) {
   uint64_t header_size{read_varint(db).first};
 
   // read serial types
-  std::vector<uint64_t> serial_types{};
+  std::vector<SerialType> serial_types{};
   serial_types.reserve(static_cast<size_t>(header_size - 1));
   int count{};
   while (count < header_size - 1) {
-    const auto [result, bytes_read] {read_varint(db)};
-    serial_types.emplace_back(result);
+    const auto [result, bytes_read]{read_varint(db)};
+    serial_types.emplace_back(SerialType{result});
     count += bytes_read;
   }
 
@@ -35,59 +52,83 @@ inline Record read_record(std::ifstream &db) {
   Values values{};
   for (const auto &t : serial_types) {
     Value value{};
-    if (t == 0) {
+    switch (t) {
+    case SerialType::NIL:
       value = std::monostate{};
-    } else if (t == 1) {
+      break;
+    case SerialType::BIT_8:
       value = read<int8_t>(db);
-    } else if (t == 2) {
+      break;
+    case SerialType::BIT_16:
       value = read<int16_t>(db);
-    } else if (t == 3) {
-      std::array<int8_t, 3> arr{};
-      for (size_t i{}; i < arr.size(); ++i) {
+      break;
+    case SerialType::BIT_24: {
+      std::array<int8_t, 3> arr_24{};
+      for (size_t i{}; i < arr_24.size(); ++i) {
         int8_t mask{static_cast<int8_t>(0b1111'1111)};
         int8_t curr{read<int8_t>(db)};
         int8_t byte{static_cast<int8_t>(curr | mask)};
-        arr[i] = byte;
+        arr_24[i] = byte;
       }
-      value = arr;
-    } else if (t == 4) {
-      value = read<int32_t>(db);
-    } else if (t == 5) {
-      std::array<int8_t, 5> arr{};
-      for (size_t i{}; i < arr.size(); ++i) {
-        int8_t mask{static_cast<int8_t>(0b1111'1111)};
-        int8_t curr{read<int8_t>(db)};
-        int8_t byte{static_cast<int8_t>(curr | mask)};
-        arr[i] = byte;
-      }
-      value = arr;
-    } else if (t == 6) {
-      value = read<int64_t>(db);
-    } else if (t == 7) {
-      // value = Double{read<Double>(db)};
-    } else if (t == 8 || t == 9 || t == 10 || t == 11) {
-      // TODO: handle missing types
-    } else if (t >= 12 && t % 2 == 0) {
-      const size_t size{static_cast<size_t>((t - 12) / 2)};
-      std::vector<int8_t> blob{};
-      blob.reserve(size);
-      for (size_t i{}; i < size; ++i) {
-        char c{};
-        db.read(&c, 1);
-        blob.emplace_back(c);
-      }
-      value = blob;
-    } else if (t >= 13 && t % 2 == 1) {
-      const size_t size{static_cast<size_t>((t - 13) / 2)};
-      std::string str{};
-      str.reserve(size);
-      for (size_t i{}; i < size; ++i) {
-        char c{};
-        db.read(&c, 1);
-        str += c;
-      }
-      value = str;
+      value = arr_24;
+      break;
     }
+    case SerialType::BIT_32:
+      value = read<int32_t>(db);
+      break;
+    case SerialType::BIT_48: {
+      std::array<int8_t, 5> arr_48{};
+      for (size_t i{}; i < arr_48.size(); ++i) {
+        int8_t mask{static_cast<int8_t>(0b1111'1111)};
+        int8_t curr{read<int8_t>(db)};
+        int8_t byte{static_cast<int8_t>(curr | mask)};
+        arr_48[i] = byte;
+      }
+      value = arr_48;
+      break;
+    }
+    case SerialType::BIT_64:
+      value = read<int64_t>(db);
+      break;
+    case SerialType::FLOAT:
+      // TODO: need to handle double in read function
+      // value = read<double>(db);
+      break;
+    case SerialType::ZERO:
+      break;
+    case SerialType::ONE:
+      break;
+    case SerialType::RESERVED_1:
+      break;
+    case SerialType::RESERVED_2:
+      break;
+    default: {
+      uint64_t other_value{static_cast<uint64_t>(t)};
+      if (other_value >= 12 && other_value % 2 == 0) {
+        const size_t size{static_cast<size_t>((other_value - 12) / 2)};
+        std::vector<int8_t> blob{};
+        blob.reserve(size);
+        for (size_t i{}; i < size; ++i) {
+          char c{};
+          db.read(&c, 1);
+          blob.emplace_back(c);
+        }
+        value = blob;
+      } else if (other_value >= 13 && other_value % 2 == 1) {
+        const size_t size{static_cast<size_t>((other_value - 13) / 2)};
+        std::string str{};
+        str.reserve(size);
+        for (size_t i{}; i < size; ++i) {
+          char c{};
+          db.read(&c, 1);
+          str += c;
+        }
+        value = str;
+      }
+      break;
+    }
+    }
+
     values.emplace_back(value);
   }
   return {
